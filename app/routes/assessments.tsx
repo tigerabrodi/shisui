3c7e7bf339bd9c120819e23659a128a75d33f1f8
tigerabrodi <tigerabrodi@gmail.com>
2021-12-13 18:05:43 +0100

    fix(assessments): rename links to bottom link and remove unnecessary styling

--- app/routes/assessments.tsx
+++ app/routes/assessments.tsx
@@ -1,10 +1,4 @@
-import {
-  Link as RemixLink,
-  LoaderFunction,
-  Outlet,
-  redirect,
-  useLocation,
-} from "remix";
+import { Link, LoaderFunction, Outlet, redirect, useLocation } from "remix";
 import { authenticator } from "~/auth/auth.server";
 
 export const loader: LoaderFunction = async ({ request }) => {
@@ -19,14 +13,14 @@ export const loader: LoaderFunction = async ({ request }) => {
   return null;
 };
 
-const Link: React.FC<{ to: string }> = ({ to, children }) => (
-  <RemixLink
+const BottomLink: React.FC<{ to: string }> = ({ to, children }) => (
+  <Link
     to={to}
     prefetch="intent"
     className="bg-black text-white text-lg w-24 h-9 font-sans flex items-center justify-center rounded-sm shadow-hover transition-all md:w-32 md:h-14 md:text-2xl"
   >
     {children}
-  </RemixLink>
+  </Link>
 );
 
 export default function Assessments() {
@@ -35,11 +29,11 @@ export default function Assessments() {
 
   return (
     <main className="h-full w-full flex-col-center pb-6 md:pb-28">
-      <h1 className="heading-one mt-2 md:mt-2">Assessments</h1>
+      <h1 className="heading-one mt-2">Assessments</h1>
       <Outlet />
       <div className="flex items-center mt-auto gap-10 md:gap-44">
-        <Link to={`/${typeOfAssessment}/new`}>Write</Link>
-        <Link to={`/${typeOfAssessment}/questions`}>Questions</Link>
+        <BottomLink to={`/${typeOfAssessment}/new`}>Write</BottomLink>
+        <BottomLink to={`/${typeOfAssessment}/questions`}>Questions</BottomLink>
       </div>
     </main>
   );
