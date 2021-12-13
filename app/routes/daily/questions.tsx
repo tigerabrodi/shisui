0165367fcd24931de85b96e1f4e3dd1c51d74b7a
tigerabrodi <tigerabrodi@gmail.com>
2021-12-13 18:06:16 +0100

    refactor(daily.questions): extract common styles into utils

--- app/routes/daily/questions.tsx
+++ app/routes/daily/questions.tsx
@@ -5,9 +5,7 @@ import { Trash } from "~/icons/Delete";
 export default function Questions() {
   return (
     <>
-      <h2 className="font-normal text-sm text-black text-center mt-3 md:text-2xl md:mt-6">
-        Questions for your daily assessments.
-      </h2>
+      <h2 className="heading-two">Questions for your daily assessments.</h2>
       <div className="w-full flex items-center justify-between mt-5 first-of-type:mt-8 md:first-of-type:mt-10 md:mt-8">
         <label htmlFor="question" className="sr-only">
           Question
@@ -18,32 +16,21 @@ export default function Questions() {
           placeholder="How was your diet today?"
           className="text-white bg-black pl-2 text-left font-bold font-serif text-sm w-60 h-7 md:w-9/12 md:h-11 md:text-xl"
         />
-        <button
-          aria-label="Delete question"
-          className="w-[38px] h-7 bg-black flex-center shadow-hover transition-all md:w-2/12 md:h-11 button-active"
-        >
-          <Trash className="w-4 h-4 md:w-6 md:h-6" />
+        <button aria-label="Delete question" className="question-button">
+          <Trash className="question-icon" />
         </button>
       </div>
       <div className="w-full flex items-center justify-end mt-5 md:mt-8">
-        <button
-          aria-label="Add Question"
-          className="w-[38px] h-7 bg-black flex-center shadow-hover transition-all md:w-2/12 md:h-11 button-active"
-        >
-          <Add className="w-4 h-4 md:w-6 md:h-6" />
+        <button aria-label="Add Question" className="question-button">
+          <Add className="question-icon" />
         </button>
       </div>
 
       <div className="mt-auto w-60 flex items-center justify-between pb-10 pt-8 md:w-5/6 md:pb-32">
-        <Link
-          to="/assessments/daily"
-          className="shadow-hover transition-all w-[85px] h-9 bg-black text-white flex-center font-medium text-lg md:w-32 md:text-3xl md:h-14"
-        >
+        <Link to="/assessments/daily" className="bottom-button-link">
           Cancel
         </Link>
-        <button className="shadow-hover w-[85px] transition-all h-9 bg-black text-white flex-center font-medium text-lg md:w-32 md:text-3xl md:h-14 button-active">
-          Save
-        </button>
+        <button className="bottom-button-link button-active">Save</button>
       </div>
     </>
   );
