dec0cef89f7dd6892bd3b7e69cdcfd65a3df20ac
tigerabrodi <tigerabrodi@gmail.com>
2021-12-14 08:18:20 +0100

    feat(daily.questions): core functionality and form validation

--- app/routes/daily/questions.tsx
+++ app/routes/daily/questions.tsx
@@ -1,37 +1,120 @@
-import { Link } from "remix";
-import { Add } from "~/icons/Add";
-import { Trash } from "~/icons/Delete";
+import {
+  ActionFunction,
+  Form,
+  Link,
+  LoaderFunction,
+  useActionData,
+  useLoaderData,
+} from 'remix'
+import { v4 } from 'uuid'
+import * as React from 'react'
+import { authenticator } from '~/auth/auth.server'
+import { Add } from '~/icons/Add'
+import { Question } from '@prisma/client'
+import { db } from '~/db/db.server'
+import { QuestionInput } from '~/components/QuestionInput'
+import toast from 'react-hot-toast'
+
+type ActionData = {
+  isError?: boolean
+}
+
+export const action: ActionFunction = async ({
+  request,
+}): Promise<Response | ActionData | undefined> => {
+  const user = await authenticator.isAuthenticated(request, {
+    failureRedirect: '/login',
+  })
+
+  const form = await request.formData()
+  const allQuestions = form.getAll('question')
+  const isAnyQuestionsEmpty = allQuestions.some((question) => !question)
+
+  if (isAnyQuestionsEmpty) {
+    return { isError: true }
+  }
+
+  /*   await db.question.deleteMany({
+    where: {
+      userId: user.id,
+      type: 'DAILY',
+    },
+  }) */
+}
+
+export const loader: LoaderFunction = async ({ request }) => {
+  const user = await authenticator.isAuthenticated(request, {
+    failureRedirect: '/login',
+  })
+
+  const dbQuestions = await db.question.findMany({
+    where: {
+      userId: user.id,
+      type: 'DAILY',
+    },
+  })
+
+  const questions = dbQuestions.length ? dbQuestions : [{ id: v4(), title: '' }]
+
+  return questions
+}
 
 export default function Questions() {
+  const actionData = useActionData<ActionData>()
+  const loaderQuestions = useLoaderData<Question[]>()
+  const [questions, setQuestions] = React.useState<Question[]>(loaderQuestions)
+
+  const deleteQuestion = (questionId: number) => {
+    setQuestions(questions.filter((question) => question.id !== questionId))
+  }
+
+  const addQuestion = () => {
+    setQuestions([...questions, { id: v4(), title: '' }] as Question[])
+  }
+
+  React.useEffect(() => {
+    if (actionData?.isError) {
+      toast.error('Please fill in all the questions you added.')
+      actionData.isError = false
+    }
+  }, [actionData?.isError])
+
   return (
     <>
       <h2 className="heading-two">Questions for your daily assessments.</h2>
-      <div className="w-full flex items-center justify-between mt-5 first-of-type:mt-8 md:first-of-type:mt-10 md:mt-8">
-        <label htmlFor="question" className="sr-only">
-          Question
-        </label>
-        <input
-          id="question"
-          type="text"
-          placeholder="How was your diet today?"
-          className="text-white bg-black pl-2 text-left font-bold font-serif text-sm w-60 h-7 md:w-9/12 md:h-11 md:text-xl"
-        />
-        <button aria-label="Delete question" className="question-button">
-          <Trash className="question-icon" />
-        </button>
-      </div>
-      <div className="w-full flex items-center justify-end mt-5 md:mt-8">
-        <button aria-label="Add question" className="question-button">
-          <Add className="question-icon" />
-        </button>
-      </div>
+      <Form
+        action="/daily/questions"
+        method="post"
+        className="w-full h-full flex-col-center"
+      >
+        {questions.map((question, index) => (
+          <QuestionInput
+            key={question.id}
+            order={index + 1}
+            question={question}
+            deleteQuestion={deleteQuestion}
+          />
+        ))}
+        <div className="w-full flex items-center justify-end mt-5 md:mt-8">
+          <button
+            aria-label="Add question"
+            className="question-button"
+            type="button"
+            onClick={() => addQuestion()}
+          >
+            <Add className="question-icon" />
+          </button>
+        </div>
 
-      <div className="mt-auto w-60 flex items-center justify-between pb-10 pt-8 md:w-5/6 md:pb-32">
-        <Link to="/assessments/daily" className="bottom-button-link">
-          Cancel
-        </Link>
-        <button className="bottom-button-link button-active">Save</button>
-      </div>
+        <div className="mt-auto w-60 flex items-center justify-between pb-10 pt-8 md:w-5/6 md:pb-32">
+          <Link to="/assessments/daily" className="bottom-button-link">
+            Cancel
+          </Link>
+          <button className="bottom-button-link button-active" type="submit">
+            Save
+          </button>
+        </div>
+      </Form>
     </>
-  );
+  )
 }
