2222239d25ba1b20bcbbafae47f4d95525618f39
tigerabrodi <tigerabrodi@gmail.com>
2021-12-14 21:36:31 +0100

    remove playwright yolo

--- app/components/AssessmentFeed.tsx
+++ app/components/AssessmentFeed.tsx
@@ -1,10 +1,10 @@
-import * as React from "react";
-import { AssessmentItem } from "./AssessmentItem";
+import * as React from 'react'
+import { AssessmentItem } from './AssessmentItem'
 
 export const AssessmentFeed: React.FC = () => {
   return (
     <ul className="mt-6 flex flex-col items-center gap-4 max-h-80 overflow-auto md:w-3/5 lg:w-2/5 xl:w-2/6 md:mt-12 md:max-h-[500px] xl:max-h-[600px]">
       <AssessmentItem />
     </ul>
-  );
-};
+  )
+}
