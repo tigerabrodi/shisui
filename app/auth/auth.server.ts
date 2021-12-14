8cc6e9bbe3953c5916ca6ec951c739aabbba5c8d
tigerabrodi <tigerabrodi@gmail.com>
2021-12-14 20:50:29 +0100

    refactor: auth session server

--- app/auth/auth.server.ts
+++ app/auth/auth.server.ts
@@ -1,21 +1,21 @@
-import { User } from "@prisma/client";
-import { Authenticator, GoogleProfile, GoogleStrategy } from "remix-auth";
-import { findOrCreateUser } from "~/db/db-operations";
-import { sessionStorage } from "./session.server";
+import { User } from '@prisma/client'
+import { Authenticator, GoogleProfile, GoogleStrategy } from 'remix-auth'
+import { findOrCreateUser } from '~/db/db-operations'
+import { authSessionStorage } from './session.server'
 
 export async function login(profile: GoogleProfile): Promise<User> {
-  const user = await findOrCreateUser(profile.id);
-  return user;
+  const user = await findOrCreateUser(profile.id)
+  return user
 }
 
-export const authenticator = new Authenticator<User>(sessionStorage);
+export const authenticator = new Authenticator<User>(authSessionStorage)
 
 if (!process.env.GOOGLE_CLIENT_ID) {
-  throw new Error("Missing GOOGLE_CLIENT_ID env");
+  throw new Error('Missing GOOGLE_CLIENT_ID env')
 }
 
 if (!process.env.GOOGLE_CLIENT_SECRET) {
-  throw new Error("Missing GOOGLE_CLIENT_SECRET env");
+  throw new Error('Missing GOOGLE_CLIENT_SECRET env')
 }
 
 authenticator.use(
@@ -23,8 +23,8 @@ authenticator.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:3000/auth/google/callback",
+      callbackURL: 'http://localhost:3000/auth/google/callback',
     },
     async (_, __, ___, profile) => login(profile)
   )
-);
+)
--- app/auth/session.server.ts
+++ app/auth/session.server.ts
@@ -1,15 +1,15 @@
-import { createCookieSessionStorage } from "remix";
+import { createCookieSessionStorage } from 'remix'
 
-export const sessionStorage = createCookieSessionStorage({
+export const authSessionStorage = createCookieSessionStorage({
   cookie: {
-    name: "_session",
-    sameSite: "lax",
-    path: "/",
+    name: '_session',
+    sameSite: 'lax',
+    path: '/',
     httpOnly: true,
-    secrets: ["s3cr3t"],
-    secure: process.env.NODE_ENV === "production",
+    secrets: [process.env.AUTH_SECRET!],
+    secure: process.env.NODE_ENV === 'production',
     maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year
   },
-});
+})
 
-export const { getSession, commitSession, destroySession } = sessionStorage;
+export const { getSession, commitSession, destroySession } = authSessionStorage
