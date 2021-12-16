412fc9a94d564729e5669e22571cadff053708a0
tigerabrodi <tigerabrodi@gmail.com>
2021-12-16 07:36:18 +0100

    refactor(AssessmentItem): extract route and year

--- app/components/AssessmentItem.tsx
+++ app/components/AssessmentItem.tsx
@@ -5,16 +5,20 @@ import { Assessment } from '~/lib/types'
 export const AssessmentItem: React.FC<{ assessment: Assessment }> = ({
   assessment,
 }) => {
+  const assessmentDetailRoute = `/${assessment.type.toLowerCase()}/${
+    assessment.id
+  }`
+  const assessmentYear = assessment.createdAt.split(',')[2].trim()
   return (
     <li className="w-72 min-h-[26px] rounded-sm flex items-center pl-2 relative bg-black md:w-full md:min-h-[40px]">
       <Link
-        to={`/${assessment.type.toLowerCase()}/${assessment.id}`}
+        to={assessmentDetailRoute}
         className="text-white font-sans text-xs font-normal hover:underline md:text-lg"
       >
         Written on {assessment.createdAt}
       </Link>
       <span className="absolute top-[3px] right-[3px] bg-white px-1 text-black rounded-sm font-normal text-[10px] md:text-base md:top-1 md:right-1">
-        {assessment.createdAt.split(',')[2].trim()}
+        {assessmentYear}
       </span>
     </li>
   )
