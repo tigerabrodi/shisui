146826de7aa3c71e1dbb1adcc31c26821fb28828
tigerabrodi <tigerabrodi@gmail.com>
2021-12-17 05:56:29 +0100

    fix(AssessmentFeed): add key to items

--- app/components/AssessmentFeed.tsx
+++ app/components/AssessmentFeed.tsx
@@ -8,7 +8,7 @@ export const AssessmentFeed: React.FC<{ assessments: Assessment[] }> = ({
   return (
     <ul className="mt-6 flex flex-col items-center gap-4 max-h-80 overflow-auto md:w-3/5 lg:w-2/5 xl:w-2/6 md:mt-12 md:max-h-[500px] xl:max-h-[600px]">
       {assessments.map((assessment) => (
-        <AssessmentItem assessment={assessment} />
+        <AssessmentItem assessment={assessment} key={assessment.id} />
       ))}
     </ul>
   )
