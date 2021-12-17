767652f7dce0a0b5cc941c3bcfcdd9005beec2da
theo <me@t3.gg>
2021-12-17 13:19:53 -0800

    New template

--- remix.config.js
+++ remix.config.js
@@ -5,6 +5,5 @@ module.exports = {
   appDirectory: "app",
   browserBuildDirectory: "public/build",
   publicPath: "/build/",
-  serverBuildDirectory: "build",
-  devServerPort: 8002
+  serverBuildDirectory: "api/build"
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,7 @@
-// @ts-nocheck
-/** @type {import('tailwindcss/tailwind-config').TailwindConfig} */
 module.exports = {
   content: ["./app/**/*.{ts,tsx}"],
   theme: {
-    fontFamily: {
-      sans: `'Lato', sans-serif`,
-      serif: `'Merriweather', serif`,
-    },
-    extend: {
-      colors: {
-        white: "#EBE1E1",
-        black: "#282424",
-      },
-    },
+    extend: {},
   },
+  plugins: [],
 };
