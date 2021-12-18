ac22bd314b3061e5548f9b11284553c74a432617
tigerabrodi <tigerabrodi@gmail.com>
2021-12-18 08:26:48 +0100

    await serve tailwind

--- app/routes/tailwindcss.tsx
+++ app/routes/tailwindcss.tsx
@@ -1,4 +1,5 @@
 import type { LoaderFunction } from 'remix'
 import { serveTailwindCss } from 'remix-tailwind'
 
-export const loader: LoaderFunction = () => serveTailwindCss('app/tailwind.css')
+export const loader: LoaderFunction = async () =>
+  await serveTailwindCss('app/tailwind.css')
