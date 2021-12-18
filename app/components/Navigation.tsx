0ea955e725981bcb42e4953b1989026866593b22
tigerabrodi <tigerabrodi@gmail.com>
2021-12-18 11:08:01 +0100

    fix(Navigation): better active styles

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -1,9 +1,13 @@
 import * as React from 'react'
-import { NavLink as RemixNavLink } from 'remix'
+import { NavLink as RemixNavLink, useLocation } from 'remix'
 
-const NavLink: React.FC<{ to: string }> = ({ to, children }) => (
+const NavLink: React.FC<{ to: string; isActive: boolean }> = ({
+  to,
+  children,
+  isActive,
+}) => (
   <RemixNavLink
-    className={({ isActive }) =>
+    className={
       isActive
         ? 'font-serif font-bold text-black text-lg underline md:text-2xl'
         : 'font-serif font-normal text-black text-lg md:text-2xl hover:underline'
@@ -16,11 +20,27 @@ const NavLink: React.FC<{ to: string }> = ({ to, children }) => (
 )
 
 export const Navigation = () => {
+  const location = useLocation()
   return (
     <nav className="min-h-[64px] w-full flex justify-between items-center px-5 md:min-h-[96px] md:px-24 md:w-4/5 lg:w-3/5 xl:w-2/5">
-      <NavLink to="/assessments/daily">Daily</NavLink>
-      <NavLink to="/assessments/weekly">Weekly</NavLink>
-      <NavLink to="/assessments/monthly">Monthly</NavLink>
+      <NavLink
+        to="/assessments/daily"
+        isActive={location.pathname.includes('daily')}
+      >
+        Daily
+      </NavLink>
+      <NavLink
+        to="/assessments/weekly"
+        isActive={location.pathname.includes('weekly')}
+      >
+        Weekly
+      </NavLink>
+      <NavLink
+        to="/assessments/monthly"
+        isActive={location.pathname.includes('monthly')}
+      >
+        Monthly
+      </NavLink>
     </nav>
   )
 }
