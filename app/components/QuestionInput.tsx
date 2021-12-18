5c4eefead9ecef897685ac610362f8c698e6e321
tigerabrodi <tigerabrodi@gmail.com>
2021-12-18 10:53:32 +0100

    fix(QuestionInput): placeholder value

--- app/components/QuestionInput.tsx
+++ app/components/QuestionInput.tsx
@@ -15,6 +15,17 @@ export const QuestionInput: React.FC<Props> = ({
   order,
   deleteQuestion,
 }) => {
+  const dailyPlaceholder = 'How was your diet today?'
+  const weeklyPlaceholder = 'How was your diet this week?'
+  const monthlyPlaceholder = 'How was your diet this month?'
+
+  const placeholder =
+    question.type === 'DAILY'
+      ? dailyPlaceholder
+      : question.type === 'WEEKLY'
+      ? weeklyPlaceholder
+      : monthlyPlaceholder
+
   return (
     <div className="w-full flex items-center justify-between mt-5 first-of-type:mt-8 md:first-of-type:mt-10 md:mt-8">
       <label htmlFor={`question-${question.id}`} className="sr-only">
@@ -25,7 +36,7 @@ export const QuestionInput: React.FC<Props> = ({
         name="question"
         type="text"
         defaultValue={question.title}
-        placeholder="How was your diet today?"
+        placeholder={placeholder}
         className="text-white bg-black pl-2 text-left font-bold font-serif text-sm w-60 h-7 md:w-9/12 md:h-11 md:text-xl"
       />
       <button
