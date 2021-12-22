72d710fd48b3f7c06aa5048dbb2953ad255cb3ef
tigerabrodi <tigerabrodi@gmail.com>
2021-12-23 00:54:49 +0100

    refactor(QuestionAnswerItem): improve typings

--- app/components/QuestionAnswerItem.tsx
+++ app/components/QuestionAnswerItem.tsx
@@ -2,11 +2,10 @@ import { QuestionAnswer } from '@prisma/client'
 import * as React from 'react'
 
 export const QuestionAnswerItem: React.FC<{
-  questionAnswer: QuestionAnswer
-}> = ({ questionAnswer: { answer, question, id } }) => {
+  questionAnswer: Pick<QuestionAnswer, 'answer' | 'question'>
+}> = ({ questionAnswer: { answer, question } }) => {
   return (
     <article
-      key={id}
       className={`w-full min-h-[60px] mt-6 flex flex-col justify-between items-start md:min-h-[80px] md:mt-12`}
     >
       <h3 className="font-bold font-serif text-lg text-black mb-2 md:text-2xl">
